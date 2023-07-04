3ac5cbdc3bebe21cbe583a376cb7c3ae50b99838
Srdjan Kokot <kokotsrdjan@gmail.com>
2023-07-04 10:14:37 +0200

    delete all

--- shared/src/commonMain/kotlin/co/cvik/user/datasource/network/KtorClientFactory.kt
+++
@@ -1,7 +0,0 @@
-package co.cvik.user.datasource.network
-
-import io.ktor.client.HttpClient
-
-expect class KtorClientFactory() {
-    fun build(): HttpClient
-}
--- shared/src/iosMain/kotlin/co/cvik/user/datasource/App.kt
+++
@@ -1,8 +0,0 @@
-package co.cvik.user.datasource
-
-import androidx.compose.ui.window.ComposeUIViewController
-import co.cvik.user.App
-
-fun MainViewController() = ComposeUIViewController {
-    App()
-}
